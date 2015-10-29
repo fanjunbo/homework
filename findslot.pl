49d5f0348cc7f0d6fc78f8d270284c24cf7e029b
fanjunbo <1564058542@qq.com>
2015-10-29 22:38:50 +0800

    Update findslot.pl

--- findslot.pl
+++ findslot.pl
@@ -11,7 +11,7 @@ generate_all_n(Acc,L,R):-length(Acc,Len), Len<L, generate_all_n([n|Acc], L, R).
 %% Count the number of a in a particular timeslot.
 count(_, [], 0) :- !.
 count(X, [X|T], N) :- count(X, T, N2), N is N2 + 1.     
-count(X, [Y|T], N) :- X \= Y,count(X, T, N).
+count(X, [Y|T], N) :- X\=Y, count(X, T, N).
 
 %% Replace nth element in a list with a given element.
 replace([_|T], 0, X, [X|T]).
@@ -35,14 +35,14 @@ loop_entry(T,S,Ini,Floor,F):-replace(T,Ini,n,Tt), modify(S,Tt,Ini,Ttt), NewIni i
 
 %% Reverse a list.
 reverse([],Z,Z).
-reverse([H|T], Z, Acc):-reverse(T, Z, [H|Acc]).
+reverse([H|T],Z,Acc):-reverse(T,Z,[H|Acc]).
 
 %% Check if all given variables are equal.
 is_equal(Var,Var,Var).
 
 %% Find the overlap timeslot of two tutors.
 find_2ts_overlap([],[],R,R).
-find_2ts_overlap([H1|T1], [H2|T2], Acc, R):-is_equal(H1,H2,a)->find_2ts_overlap(T1,T2,[a|Acc],R); find_2ts_overlap(T1,T2,[n|Acc],R).
+find_2ts_overlap([H1|T1],[H2|T2],Acc,R):-is_equal(H1,H2,a)->find_2ts_overlap(T1,T2,[a|Acc],R); find_2ts_overlap(T1,T2,[n|Acc],R).
 
 find_overlap(R,[],R).
 find_overlap(X,Y,Z):-find_2ts_overlap(X,Y,[],R), reverse(R,Z,[]).
@@ -55,4 +55,4 @@ find_tt_overlap([H|T],Acc,R):-get_adjtt_overlap(H,Acc,Ac), find_tt_overlap(T,Ac,
 
 
 find_time_slots(T,S,Num,Tuto):-find_tt_overlap(T,[],Tt), if_fit_student(S,Tt)->length(Tt, Ini), loop_entry(Tt,S,Ini,-1,Tuto), count(a,Tuto,Num); 
-	Num is 0,find_tt_overlap(T,[],Tt), length(Tt,Ini), generate_all_n([], Ini,Tuto).
+	Num is 0, find_tt_overlap(T,[],Tt), length(Tt,Ini), generate_all_n([], Ini,Tuto).
